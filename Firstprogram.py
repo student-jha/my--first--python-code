7cd40d0244caf94ab31a65c3880e98486daba556
student-jha <166119371+student-jha@users.noreply.github.com>
2024-04-05 15:31:43 +0530

    Add files via upload

---
+++ Firstprogram.py
@@ -0,0 +1,121 @@
+#marks = int(input("enter the marks: "))
+#if(marks >= 90):
+#    grade = 'A'
+#elif(marks >=80 and marks < 90):
+#    grade = "B"
+#elif(marks >= 70 and marks < 80):
+#    grade = 'C'
+#else:
+#    grade = 'D'
+
+#print("grade of the student ->" , grade)
+
+#nesting
+#age = 90
+#if(age >= 18):
+#    if(age >= 80):
+#        print("cannot drive")
+#    else:
+#        print("can drive")
+#else:
+#    print("cannot drive")
+
+
+#num = int(input("enter number: "))
+#rem = num % 2 
+#if(rem == 0):
+#    print("even")
+#else:
+#    print("odd")
+
+#a = int(input("enter the first number: "))    
+#b = int(input("enter the second number: ")) 
+#c= int(input("enter the third number: ")) 
+#if(a >= b and a >= c):
+#    print("first number is largest", a)
+#elif(b >= c):
+#    print("second number is largest", b)
+#else:
+#    print("third number is largest", c) 
+
+#a = int(input("enter first number:"))
+#b = int(input("enter second number:"))
+#c = int(input("enter third number:"))
+#d = int(input("enter fourth number:"))
+
+#if(a >= b and a >= c and a >= d):
+#    print("first number largest", a)
+#elif(b >= c and b >= d):
+#    print("second number largest", b)
+#elif(c >= d):
+#    print("third number largest", c) 
+#else: 
+#    print("fourth number largest", d)      
+
+x = int(input("enter the number: "))
+ 
+if(x % 7 == 0):
+    print("multiple of 7 ")
+else:
+    print("not multiple")     
+
+
+
+
+
+a = 12
+a.__new__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
